e3d1babd75e4f4f2ba83669710e2c684b5eeae53
gosa_dadi <gosa.d.dadi@gmail.com>
2022-12-28 16:57:12 -0800

    updated

--- src/main/java/com/exploreBooks/springbootlibrary/controller/MessagesController.java
+++ src/main/java/com/exploreBooks/springbootlibrary/controller/MessagesController.java
@@ -7,7 +7,7 @@ import com.exploreBooks.springbootlibrary.utils.ExtractJWT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("https://discover-a-book.vercel.app")
+@CrossOrigin(origins = {"http://localhost:3000", "https://inventory-app-api.vercel.app"})
 @RestController
 @RequestMapping("/api/messages")
 public class MessagesController {
@@ -25,7 +25,7 @@ public class MessagesController {
     }
     @PutMapping("/secure/admin/message")
     public void putMessage(@RequestHeader(value = "Authorization") String token,
-                           @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
+                        @RequestBody AdminQuestionRequest adminQuestionRequest) throws Exception {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
         if (admin == null || !admin.equals("admin")) {
